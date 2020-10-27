b5b6a0525667fdd0af2d6b980f8ed24a7bdbb920
Tung HOANG <tung.hoang@gmail.com>
2020-10-27 18:29:15 +0700

    regen fix idProjectStudentRel and idProjectAdvisorRel

--- apis/projectStudentRels/model.py
+++ apis/projectStudentRels/model.py
@@ -2,7 +2,7 @@ from flask_restplus.fields import Integer, Float, String, String as Text, Date, 
 
 def create_model(api):
   model = api.model('projectStudentRel', {
-    'idProjectStudentRel': Integer,
+    'idProjectstudentrel': Integer,
     'idStudent': Integer,
     'idProject': Integer,
     'status': Integer 
